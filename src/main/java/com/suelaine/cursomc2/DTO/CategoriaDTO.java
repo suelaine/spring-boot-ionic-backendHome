f882428889b999b28672ebf28f15f091644f0f39
suelaine <suelaine.andrade@gmail.com>
2018-09-08 09:42:14 -0300

    Correções : update  em CategoruaService e construtor de Pagamento

--- src/main/java/com/suelaine/cursomc2/DTO/CategoriaDTO.java
+++ src/main/java/com/suelaine/cursomc2/DTO/CategoriaDTO.java
@@ -14,7 +14,7 @@ public class CategoriaDTO implements Serializable{
 	private Integer id;
 	
 	@NotEmpty(message="Preenchimento Obrigatório")
-	@Length(min=5,max=80,message="O tamanhod eve ser entre 5 e 80 caracteres")
+	@Length(min=5,max=80,message="O tamanhod eve ser entre 5 e 80 caracteres!")
 	private String nome;
 	
 	public CategoriaDTO() {}
